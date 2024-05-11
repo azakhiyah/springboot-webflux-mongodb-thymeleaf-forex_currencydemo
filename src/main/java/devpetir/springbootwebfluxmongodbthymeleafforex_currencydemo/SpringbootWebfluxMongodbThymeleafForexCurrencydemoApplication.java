5c6588b029582223d15f7ea45fb9626bf2e2681e
zakhiyah <azakhiyah@gmail.com>
2024-05-11 22:41:21 +0700

    firstcommit

---
+++ src/main/java/devpetir/springbootwebfluxmongodbthymeleafforex_currencydemo/SpringbootWebfluxMongodbThymeleafForexCurrencydemoApplication.java
@@ -0,0 +1,13 @@
+package devpetir.springbootwebfluxmongodbthymeleafforex_currencydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebfluxMongodbThymeleafForexCurrencydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebfluxMongodbThymeleafForexCurrencydemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devpetir/springbootwebfluxmongodbthymeleafforex_currencydemo/SpringbootWebfluxMongodbThymeleafForexCurrencydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package devpetir.springbootwebfluxmongodbthymeleafforex_currencydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebfluxMongodbThymeleafForexCurrencydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
